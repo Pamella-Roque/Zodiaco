4018a8591e6d8ea53892896682f4ac54fa44f197
unknown <oliveirapamella327@gmail.com>
2025-05-21 20:40:48 -0300

    criando homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterOutlet, RouterLink],
+  template: `
+    <nav class="navbar">
+      <div class="nav-content">
+          <a routerLink="/"  class="logo">AstroNet</a>
+      </div>
+    </nav>
+
+    <router-outlet></router-outlet>
+  `,
+  styles: [`
+    .navbar {
+      background-color: #2c3e50;
+      padding: 1rem;
+      color: white;
+    }
+    .nav-content {
+      display: flex;
+      justify-content: space-between;
+      align-items: left;
+    }
+    .nav-links {
+      list-style: none;
+      display: flex;
+      gap: 1rem;
+    }
+    
+  `]
 })
-export class AppComponent {
-  title = 'Zodiaco';
-}
+export class AppComponent {}
